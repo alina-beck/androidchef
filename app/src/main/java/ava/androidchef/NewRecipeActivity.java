325159d5a6ed2830b37e3fecc15541cdcb7cc168
AndroidAva <4ndroid4va@gmail.com>
2016-04-15 13:58:03 +0200

    created NewRecipeActivity and NewRecipeFragment with basic UI elements

---
+++ app/src/main/java/ava/androidchef/NewRecipeActivity.java
@@ -0,0 +1,20 @@
+package ava.androidchef;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+public class NewRecipeActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_recipe);
+
+        if (savedInstanceState != null) {
+            return;
+        }
+
+        NewRecipeFragment newRecipeFragment = new NewRecipeFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, newRecipeFragment).commit();
+    }
+}
---
+++ app/src/main/java/ava/androidchef/NewRecipeFragment.java
@@ -0,0 +1,20 @@
+package ava.androidchef;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NewRecipeFragment extends Fragment {
+
+    public NewRecipeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_new_recipe, container, false);
+    }
+
+}
