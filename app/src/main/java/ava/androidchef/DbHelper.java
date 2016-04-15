5ef1c613a8b330f87bc0d0b7804b2fdde32f0ff1
AndroidAva <4ndroid4va@gmail.com>
2016-04-15 10:49:46 +0200

    set up sqlite database with DbHelper

---
+++ app/src/main/java/ava/androidchef/DbHelper.java
@@ -0,0 +1,45 @@
+package ava.androidchef;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DbHelper extends SQLiteOpenHelper {
+
+    public static DbHelper singletonInstance;
+
+    public static final String DATABASE_NAME = "chef.db";
+    public static final int DATABASE_VERSION = 1;
+
+    public static final String TABLE_RECIPES = "recipes";
+
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_TITLE = "title";
+
+    // database creation SQL syntax
+    private static final String CREATE_DATABASE = "create table " + TABLE_RECIPES + "(" +
+            COLUMN_ID + " integer primary key autoincrement, " +
+            COLUMN_TITLE + " text not null);";
+
+    private DbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public static DbHelper getInstance(Context context) {
+        if (singletonInstance == null) {
+            singletonInstance = new DbHelper(context.getApplicationContext());
+        }
+        return singletonInstance;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_DATABASE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists " + TABLE_RECIPES);
+        onCreate(db);
+    }
+}
