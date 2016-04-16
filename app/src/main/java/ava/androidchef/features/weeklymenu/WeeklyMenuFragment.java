963d4ea5166f75dbdc7927e4150e6f12e19542a8
AndroidAva <4ndroid4va@gmail.com>
2016-04-16 17:28:17 +0200

    implemented WeeklyMenuPresenter, dismissed custom adapter

--- app/src/main/java/ava/androidchef/features/weeklymenu/WeeklyMenuFragment.java
+++ app/src/main/java/ava/androidchef/features/weeklymenu/WeeklyMenuFragment.java
@@ -3,13 +3,14 @@ package ava.androidchef.features.weeklymenu;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.widget.ArrayAdapter;
+
 import java.util.ArrayList;
+
 import ava.androidchef.models.Recipe;
-import ava.androidchef.models.RecipeDAO;
 
 public class WeeklyMenuFragment extends ListFragment {
 
-    private ArrayList<Recipe> recipes;
+    private WeeklyMenuPresenter presenter;
 
     public WeeklyMenuFragment() {
     }
@@ -17,12 +18,13 @@ public class WeeklyMenuFragment extends ListFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        RecipeDAO recipeDAO = new RecipeDAO(getActivity());
-        recipes = recipeDAO.fetchWeeklyMenu();
 
+        this.presenter = new WeeklyMenuPresenter(this);
+        presenter.onFragmentCreate();
+    }
+
+    public void displayWeeklyMenu(ArrayList<Recipe> recipes) {
         ArrayAdapter<Recipe> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, recipes);
         setListAdapter(adapter);
-
-        }
     }
 }
---
+++ app/src/main/java/ava/androidchef/features/weeklymenu/WeeklyMenuPresenter.java
@@ -0,0 +1,24 @@
+package ava.androidchef.features.weeklymenu;
+
+import java.util.ArrayList;
+import ava.androidchef.models.Recipe;
+import ava.androidchef.models.RecipeDAO;
+
+public class WeeklyMenuPresenter {
+
+    private WeeklyMenuFragment fragment;
+    private ArrayList<Recipe> recipes;
+
+    public WeeklyMenuPresenter(WeeklyMenuFragment fragment) {
+        this.fragment = fragment;
+    }
+
+    public void onFragmentCreate() {
+        RecipeDAO recipeDAO = new RecipeDAO(fragment.getActivity());
+        recipes = recipeDAO.fetchWeeklyMenu();
+
+        fragment.displayWeeklyMenu(recipes);
+    }
+
+
+}
