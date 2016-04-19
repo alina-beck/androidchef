d227e98cf7c2c0c0ec98b5ff4077378b4a0fb6eb
AndroidAva <4ndroid4va@gmail.com>
2016-04-19 18:02:08 +0200

    saving weekly menu to shared preferences

--- app/src/main/java/ava/androidchef/features/weeklymenu/WeeklyMenuFragment.java
+++ app/src/main/java/ava/androidchef/features/weeklymenu/WeeklyMenuFragment.java
@@ -21,8 +21,8 @@ public class WeeklyMenuFragment extends ListFragment {
 
         String buttonClicked = getActivity().getIntent().getExtras().getString("buttonClicked");
 
-        this.presenter = new WeeklyMenuPresenter(this, buttonClicked);
-        presenter.onFragmentCreate();
+        this.presenter = new WeeklyMenuPresenter(this);
+        presenter.onFragmentCreate(buttonClicked);
     }
 
     public void displayWeeklyMenu(ArrayList<Recipe> recipes) {
--- app/src/main/java/ava/androidchef/features/weeklymenu/WeeklyMenuPresenter.java
+++ app/src/main/java/ava/androidchef/features/weeklymenu/WeeklyMenuPresenter.java
@@ -1,5 +1,8 @@
 package ava.androidchef.features.weeklymenu;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 import java.util.ArrayList;
 import ava.androidchef.models.Recipe;
 import ava.androidchef.models.RecipeDAO;
@@ -7,25 +10,34 @@ import ava.androidchef.models.RecipeDAO;
 public class WeeklyMenuPresenter {
 
     private WeeklyMenuFragment fragment;
-    private String buttonClicked;
-    private ArrayList<Recipe> recipes;
 
-    public WeeklyMenuPresenter(WeeklyMenuFragment fragment, String buttonClicked) {
+    public WeeklyMenuPresenter(WeeklyMenuFragment fragment) {
         this.fragment = fragment;
-        this.buttonClicked = buttonClicked;
     }
 
-    public void onFragmentCreate() {
+    public void onFragmentCreate(String buttonClicked) {
+
+        ArrayList<Recipe> recipes;
+        SharedPreferences currentMenu = fragment.getActivity().getPreferences(Context.MODE_PRIVATE);
 
         switch (buttonClicked) {
             case "createMenu":
                 RecipeDAO recipeDAO = new RecipeDAO(fragment.getActivity());
-                this.recipes = recipeDAO.getRandomMenu(7);
+                recipes = recipeDAO.getRandomMenu(7);
 
                 fragment.displayWeeklyMenu(recipes);
+
+                SharedPreferences.Editor editor = currentMenu.edit();
+
+                for (int i = 0; i < recipes.size(); i++) {
+                    String stringMenu = recipes.get(i).getId() + "," + recipes.get(i).getTitle();
+
+                    editor.putString(Integer.toString(i), stringMenu);
+                    editor.apply();
+                }
                 break;
             case "currentMenu":
-                this.recipes = new ArrayList<>();
+                recipes = new ArrayList<>();
                 recipes.add(new Recipe("testworks"));
 
                 fragment.displayWeeklyMenu(recipes);
