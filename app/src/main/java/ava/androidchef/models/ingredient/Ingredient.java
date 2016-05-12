188ee39cb5ddb0596154ab3bd91cc8a5b061b0e3
AndroidAva <4ndroid4va@gmail.com>
2016-05-12 13:51:18 +0200

    Ingredient implements Parcelable

--- app/src/main/java/ava/androidchef/models/ingredient/Ingredient.java
+++ app/src/main/java/ava/androidchef/models/ingredient/Ingredient.java
@@ -1,6 +1,9 @@
 package ava.androidchef.models.ingredient;
 
-public class Ingredient {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Ingredient implements Parcelable {
 
     private int id;
     private String name;
@@ -32,4 +35,31 @@ public class Ingredient {
     public String getUnit() {
         return unit;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(name);
+        dest.writeString(unit);
+    }
+
+    private Ingredient(Parcel in) {
+        this.id = in.readInt();
+        this.name = in.readString();
+        this.unit = in.readString();
+    }
+
+    public static final Parcelable.Creator<Ingredient> CREATOR = new Parcelable.Creator<Ingredient>() {
+        public Ingredient createFromParcel(Parcel in) {
+            return new Ingredient(in);
+        }
+        public Ingredient[] newArray(int size) {
+            return new Ingredient[size];
+        }
+    };
 }
