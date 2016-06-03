388d07aa18df696239ced44e87b3ae8a9872a0b4
AndroidAva <4ndroid4va@gmail.com>
2016-06-03 10:32:35 +0200

    hiding soft keyboard when saving recipe

--- app/src/main/java/ava/androidchef/features/addrecipe/AddRecipeActivity.java
+++ app/src/main/java/ava/androidchef/features/addrecipe/AddRecipeActivity.java
@@ -1,8 +1,11 @@
 package ava.androidchef.features.addrecipe;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 import ava.androidchef.R;
 
@@ -24,6 +27,9 @@ public class AddRecipeActivity extends AppCompatActivity {
     }
 
     public void resetInputFields() {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        View v = getCurrentFocus();
+        imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         EnterRecipeFragment enterRecipeFragment = new EnterRecipeFragment();
         fragmentTransaction.replace(R.id.fragment_container_enter_recipe, enterRecipeFragment);
